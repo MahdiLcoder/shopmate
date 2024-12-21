60deb15468eb46d254e702dbae98870b7f6ea309
MahdiLcoder <labidimahdi95@gmail.com>
2024-12-21 17:28:15 +0100

    make remove button on the cart router interactive

--- src/components/CartCard.js
+++ src/components/CartCard.js
@@ -1,14 +1,14 @@
 import "./CartCard.css";
-
+import { useCart } from "../Context/CartContex";
 export const CartCard = ({product}) => {
   const {name, price, image} = product;
-
+  const {REMOVE_FROM_CART} = useCart();
   return (
       <div className="cartCard">
         <img src={image} alt={name} />
         <p className="productName">{name}</p>
         <p className="productPrice">${price}</p>
-        <button>Remove</button>
+        <button onClick={() => REMOVE_FROM_CART(product.id)}>Remove</button>
       </div>
   )
 }
